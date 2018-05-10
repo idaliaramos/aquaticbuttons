a102c71114be8525f864130b65da824ad8c40663
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2018-05-09 18:27:23 -0700

    added css button colors and comments

--- portfolio-sites/src/App.js
+++ portfolio-sites/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pressed_button1: true,
-      pressed_button2: true
+      pressed_button1: false,
+      pressed_button2: false
     };
   }
 
   //call back function for button 2 to change the color of button 1
   changeColor1 = event => {
-    console.log("click");
-    console.log(this.state, "state");
     this.setState({ pressed_button2: !this.state.pressed_button2 });
   };
   //call back function for button 1 to change the color of button 2
@@ -24,25 +21,27 @@ class App extends Component {
 
   render() {
     //set bgColors dependending on the state
-    let bgColor = this.state.pressed_button1 ? "black" : "white";
-    let bgColor2 = this.state.pressed_button2 ? "black" : "red";
+    let bgColor = this.state.pressed_button1 ? "#6FACC8" : "#DB9F40";
+    let bgColor2 = this.state.pressed_button2 ? "#D22E35" : "#D97C38";
     return (
       <div className="App">
         <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <h1 className="App-title">2 Button site!</h1>
+          <h1 className="App-title">2 Button Site</h1>
         </header>
+        {/* //button1, when pressed button2 will change colors */}
         <button
           className="button"
           style={{ backgroundColor: bgColor2 }}
-          onClick={this.changeColor2.bind(this)}
+          onClick={this.changeColor2}
         >
           Button1
         </button>
+
+        {/* //button2, when pressed button1 will change colors */}
         <button
           className="button"
           style={{ backgroundColor: bgColor }}
-          onClick={this.changeColor1.bind(this)}
+          onClick={this.changeColor1}
         >
           Button 2
         </button>
